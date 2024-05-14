9017dda2ca97d55a08811b6d11db54d157da3add
LucasPulgaci032 <163359576+LucasPulgaci032@users.noreply.github.com>
2024-05-14 09:12:14 -0300

    Add files via upload

---
+++ exercicio6.js
@@ -0,0 +1,43 @@
+for (let j=1; j<=10;) {
+    console.log("tabuada de"+ j);
+    for (let n=1; n<=10; n++) {
+        const result = j * n;
+        console.log(`${j} * ${n} = ${result}`);
+    }
+     console.log("o resultado é" + result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (let i=1; i<=10; i++) {
+    console.log("tabuada de" + i);
+    for (let p=1; p<=10; p++){
+        const result = i * p;
+        console.log(`${i} x ${p} = ${result}`);
+    }
+    console.log('');
+}
